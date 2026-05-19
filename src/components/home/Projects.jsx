349c9cddc4d4f9c1243ab645afb0aa7bb4cadf5f
Presha Shrestha <preshashrestha041@gmail.com>
2026-05-19 14:00:02 +0545

    Added projects content

--- src/components/home/Projects.jsx
+++ src/components/home/Projects.jsx
@@ -1,14 +1,36 @@
 import "./Projects.css";
+import { useRef } from "react";
 
 import project9 from "../../assets/project9.png";
 import project10 from "../../assets/project10.png";
 import project11 from "../../assets/project11.png";
 import project12 from "../../assets/project12.png";
+import project13 from "../../assets/project13.png";
+import project14 from "../../assets/project14.png";
+import project15 from "../../assets/project15.png";
 
 const Projects = () => {
+  const sliderRef = useRef(null);
+
+  /* RIGHT SLIDE */
+  const slideRight = () => {
+    sliderRef.current.scrollBy({
+      left: 420,
+      behavior: "smooth",
+    });
+  };
+
+  /* LEFT SLIDE */
+  const slideLeft = () => {
+    sliderRef.current.scrollBy({
+      left: -420,
+      behavior: "smooth",
+    });
+  };
+
   return (
     <section className="projects-section">
-      {/* TITLE */}
+      {/* HEADER */}
       <div className="projects-header">
         <div className="title-line"></div>
 
@@ -17,20 +39,22 @@ const Projects = () => {
         <div className="title-line"></div>
       </div>
 
-      {/* PARAGRAPH */}
+      {/* TEXT */}
       <p className="projects-text">
         Bridging 35 years of industry heritage with modern,
         international-standard technical consulting for diverse development
         projects.
       </p>
 
-      {/* PROJECT SLIDER */}
+      {/* SLIDER */}
       <div className="projects-slider">
         {/* LEFT BUTTON */}
-        <button className="slider-btn left-btn">←</button>
+        <button className="slider-btn left-btn" onClick={slideLeft}>
+          &lt;
+        </button>
 
-        {/* IMAGES */}
-        <div className="projects-wrapper">
+        {/* PROJECTS */}
+        <div className="projects-wrapper" ref={sliderRef}>
           <div className="project-card">
             <img src={project9} alt="" />
           </div>
@@ -46,10 +70,24 @@ const Projects = () => {
           <div className="project-card">
             <img src={project12} alt="" />
           </div>
+
+          <div className="project-card">
+            <img src={project13} alt="" />
+          </div>
+
+          <div className="project-card">
+            <img src={project14} alt="" />
+          </div>
+
+          <div className="project-card">
+            <img src={project15} alt="" />
+          </div>
         </div>
 
         {/* RIGHT BUTTON */}
-        <button className="slider-btn right-btn">→</button>
+        <button className="slider-btn right-btn" onClick={slideRight}>
+          &gt;
+        </button>
       </div>
 
       {/* BUTTON */}
